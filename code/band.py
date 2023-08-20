b612945c7d2e55e1ed999dc40993f3fe4ece8c5f
Josh Myer <josh@joshisanerd.com>
2023-08-19 17:23:27 -0700

    Band: adds traffic log file, extend documentation
    This is a slightly messy commit that does two things.  One is adding a
    log file that we can use to dump traffic, and the other is simply
    adding a bunch of documentation to this class.

--- code/band.py
+++ code/band.py
@@ -52,23 +52,49 @@ class BandManager(gatt.DeviceManager):
 
 class Band(gatt.Device):
     '''The GATT interface
+
+    In general, you want to create these via `Band.find_band()`.  That
+    function takes in callbacks for what to do when the connection is
+    established and when a buffer is received.  To send packet, use
+    `band.enqueue(pkt)` with a Packet object.
+
+    For the details of these, please see the appropriate documentation
+    below, or just look at the blink example.
+
+    You can optionally attach a traffic logger via
+    `attach_traffic_log`.  This takes a file-like object as an
+    argument (it just needs a write method), and logs all data on the
+    bus to that file.  This is helpful when working with new packet
+    types.  To stop the log, call attach with None.
     '''
     def __init__(self, mac_address, manager, conn_cb, rx_cb):
         super().__init__(mac_address=mac_address, manager=manager)
 
-        self.connected = False
-        self.connerr = False
-        self.enumerated = False
+        self.connected = False  # Are the characteristics connected?
+        self.connerr = False    # Have we gotten an error state
 
         self.rx_char = None
         self.tx_char = None
         self.rx_service = None
 
-        self.write_pending = False
-        self.write_buf = list()   # List of (buf, seqno) tuples, buf <20B long
+        self.write_pending = False  # Do we have a write outstanding?
+        self.write_buf = list()   # List of (buf, seqno) tuples
 
         self.conn_cb = conn_cb
         self.rx_cb = rx_cb
+
+        self.logfile = None
+
+    def attach_traffic_log(self, f):
+        '''Dump all traffic to the given file-like object
+
+        This dumps all data into the file passed in.  It will be
+        hex-encoded, with a '>' (to band) or '<' (from band) at the
+        beginning of each line.
+
+        To stop logging, pass in None
+        '''
+        self.logfile = f
 
     @classmethod
     def find_band(cls, conn_cb, rx_cb, mac_address=None, adapter_name='hci0'):
@@ -100,6 +126,9 @@ class Band(gatt.Device):
         are popped off the queue, and then we blindly try to continue
         on.
 
+        Note that this doesn't use proper mutexes, and therefore may
+        be racy in a few places.  For my purposes, this is fine, and
+        it doesn't introduce new ways to hang the program.
         '''
         manager = BandManager(adapter_name, conn_cb, rx_cb)
 
@@ -122,9 +151,11 @@ class Band(gatt.Device):
                 logging.debug(f'Skipping discovery, target MAC={mac_address}')
                 manager.run()
                 _connect(mac_address)
+
         logging.debug('find_band starting thread')
         thread = threading.Thread(target=run_gatt_manager)
         thread.start()
+
         return manager, thread
 
     def connect_succeeded(self):
@@ -147,26 +178,54 @@ class Band(gatt.Device):
         a blip and a cue to poke the manager to reenable discovery.
         '''
         super().disconnect_succeeded()
+
         self.connected = False
         self.connerr = False
+
         logging.debug("[%s] Disconnected" % (self.mac_address))
-        self.manager.found = False
+
+        self.manager.found = False  # Allow discovery to restart
 
     def characteristic_value_updated(self, characteristic, value):
+        '''(Internal) Inbound buffer callback
+
+        This is called when we have incoming BLE data.  We just
+        blindly pass it on to the `rx_cb`, but also log the data.
+        '''
+        if self.logfile:
+            self.logfile.write(f'< {value.hex()}\n')
+
         if self.rx_cb:
             self.rx_cb(characteristic, value)
         else:
             logging.debug('\t\tNotification %s: %s' % (characteristic.uuid, value.hex()))
 
-    def _pop_queue(self):
+    def _attempt_transmit(self):
+        '''(Internal) Attempt a write
+
+        This just tries to send the top buffer out to the BLE
+        connection.  It also logs data as appropriate.
+        '''
         if self.write_pending:
-            raise ValueError("Attempted to enqueue packets with an outstanding write")
+            raise ValueError("Attempted to transmit buffers with an outstanding write")
+
         buf = self.write_buf[0][0]
         self.rx_char.write_value(buf)
+
+        if self.logfile:
+            self.logfile.write(f'> {buf.hex()}\n')
+
         logging.debug(f'\tXMIT: {buf.hex()}')
+
         self.write_pending = True
 
-    def enqueue(self, pkt):
+    def enqueue(self, pkt) -> None:
+        '''Enqueue a packet-like object
+
+        pkt is a Packet instance, or at least something with a
+        `to_bytes()` method and a `header` attribute (with a `seqno`
+        attribute).
+        '''
         buf = pkt.to_bytes()
         seqno = pkt.header.seqno
 
@@ -174,19 +233,37 @@ class Band(gatt.Device):
             i1 = min(i0 + 20, len(buf))
             self.write_buf.append( (buf[i0:i1], seqno) )
 
-#        logging.debug(f'\t\tEnqueue, length now {len(self.write_buf)}: {self.write_buf.hex()}')
         if not self.write_pending:
-            self._pop_queue()
+            self._attempt_transmit()
 
     def characteristic_write_value_succeeded(self, characteristic):
+        '''(Internal) Called when a write succeeds
+
+        This gets called whenever one of our BLE writes succeeds.  We
+        use this to pop that buffer off the queue and trigger the
+        attempt to write the next one out.
+
+        # TODO Should this have a callback when it completes the write for a given seqno?
+        '''
         logging.debug(f'\tWrite succ: {characteristic.uuid}')
         self.write_pending = False
         self.write_buf.pop(0)
 
         if self.write_buf:
-            self._pop_queue()
+            self._attempt_transmit()
 
     def characteristic_write_value_failed(self, characteristic, error):
+        '''(Internal) Called when a write fails
+
+        We just log this, and then clear the rest of that seqno's
+        buffer.
+
+        TODO Should we have a callback when a seqno fails to send?
+
+        XXX TODO Should this retry the write?  I'm honestly not sure
+        what "failed" means in this context.
+
+        '''
         seqno = self.write_buf[0][1]
         logging.debug(f'\tWrite fail: {characteristic.uuid}: {seqno}: {error} :: clearing packet')
 
@@ -196,9 +273,17 @@ class Band(gatt.Device):
         self.write_pending = False
 
         if self.write_buf:
-            self._pop_queue()
+            # Attempt to keep on truckin'
+            self._attempt_transmit()
 
     def services_resolved(self):
+        '''(Internal) Called when services are resolved
+
+        This is called when our BLE link is fully established and we
+        can pull out the service and characteristics we care about.
+
+        This is where we chain off to the "fully connected" callback.
+        '''
         super().services_resolved()
 
         logging.debug("[%s] Resolved services" % (self.mac_address))
@@ -215,7 +300,6 @@ class Band(gatt.Device):
                     self.tx_char = characteristic
                     self.tx_char.enable_notifications()
 
-        self.enumerated = True
         self.connected = True
         if self.conn_cb:
             self.conn_cb(self)
