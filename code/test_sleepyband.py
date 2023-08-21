baf86e2163f38777fd79cad0385b32c5b196f49e
Josh Myer <josh@joshisanerd.com>
2023-08-20 21:26:53 -0700

    Tests: ugly commit, apologies
    This is an ugly commit, evidence too much is in flux.
    
    Removes some dead/obviated test cases
    
    Adds tests for the new log file packets
    
    Lots of style fixups to make pycodestyle happy

--- code/test_sleepyband.py
+++ code/test_sleepyband.py
@@ -2,12 +2,12 @@
 
 import os
 import sys
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-
 import unittest
 
-from packets import _crc16, Header
-from packets import AckPacket, SessionStartPacket, ConfigGetPacket, DeviceResetPacket, SendStoredDataPacket, TechnicalStatusPacket, LEDPacket, IsDevicePairedPacket, IsDevicePairedResponsePacket
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from packets import _crc16, Header  # noqa
+from packets import *  # noqa
 
 
 class TestCRC(unittest.TestCase):
@@ -42,7 +42,7 @@ class TestHeader(unittest.TestCase):
 
     def test_to_bytes__zero_crc(self):
         expected = bytes.fromhex("bbbb2a000000000000000000000000001800000000000000")
-        
+
         hdr = Header(0x2a, 0, 0, 24, 0)
 
         self.assertEqual(hdr.crc, 0)
@@ -50,7 +50,7 @@ class TestHeader(unittest.TestCase):
 
         # This shouldn't have the crc-modifying side effect
         self.assertEqual(hdr.crc, 0)
-        
+
         self.assertEqual(expected, got)
 
     def test_roundtrip(self):
@@ -69,7 +69,7 @@ class TestHeader(unittest.TestCase):
         self.assertEqual(hdr.response, got.response)
         self.assertEqual(hdr.crc, 39389)
         self.assertEqual(hdr.crc, got.crc)
-        
+
     def test_peek_len(self):
         buf = bytes.fromhex("bbbb2a000000000000000000000000001800000000006444")
         got_len = Header.peek_len(buf)
@@ -87,7 +87,6 @@ class TestHeader(unittest.TestCase):
         expected = 0x18
         self.assertEqual(expected, got_len)
 
-        
     def test_from_bytes(self):
         buf = bytes.fromhex("bbbb2a000000000000000000000000001800000000006444")
         hdr = Header.from_bytes(buf)
@@ -97,7 +96,8 @@ class TestHeader(unittest.TestCase):
         self.assertEqual(hdr.length, 0x18)
         self.assertEqual(hdr.response, 0)
         self.assertEqual(hdr.crc, 0x4464)
-            
+
+
 class TestPacket(unittest.TestCase):
     def assert_headers_equal(self, hdr, got, desc):
         self.assertEqual(hdr.kind, got.kind, f'{desc}: header command mismatch')
@@ -126,8 +126,7 @@ class TestPacket(unittest.TestCase):
 
         rt_got = pkt_cls.from_bytes(got)
         self.assert_headers_equal(pkt.header, rt_got.header, pkt_cls)
-        
-    
+
     def test_ack_packet__nack(self):
         expected = bytes.fromhex("bbbb00000000000000000000abffffff1d00000000004165f00fcd0000")
         pkt = AckPacket(0xffffffab, 0xcd, 0xf00f)
@@ -141,7 +140,7 @@ class TestPacket(unittest.TestCase):
         rt_got = AckPacket.from_bytes(got)
         self.assert_headers_equal(pkt.header, rt_got.header, "AckPacket")
         self.assertEqual(pkt.orig_kind, rt_got.orig_kind)
-        self.assertEqual(pkt.status, rt_got.status)        
+        self.assertEqual(pkt.status, rt_got.status)
 
     def test_ack_packet__succ(self):
         # And now a success case
@@ -153,19 +152,30 @@ class TestPacket(unittest.TestCase):
         self.assertEqual(0x2a, pkt.orig_kind)
         self.assertEqual(0, pkt.status)
         self.assertTrue(pkt.is_success())
-        
+
         rt_got = AckPacket.from_bytes(got)
         self.assert_headers_equal(pkt.header, rt_got.header, "AckPacket")
         self.assertEqual(pkt.orig_kind, rt_got.orig_kind)
-        self.assertEqual(pkt.status, rt_got.status)        
+        self.assertEqual(pkt.status, rt_got.status)
 
     def test_session_start_packet(self):
-        expected = bytes.fromhex("bbbb0100bc9a000000000000341200002c0000000000ecd01928374601342e322e302e363900000000000000")
+        expected = bytes.fromhex('''
+            bbbb0100bc9a000000000000341200002c0000000000
+            ecd01928374601342e322e302e363900000000000000''')
         pkt = SessionStartPacket(0x1234, 0x19283746, 1, "4.2.0.69\0\0\0\0\0\0", timestamp=0x9abc)
         got = pkt.to_bytes()
 
         self.maybe_dump_bufs(expected, got)
         self.assertEqual(expected, got)
+        self.assertEqual(0x19283746, pkt.host_id)
+        self.assertEqual(1, pkt.mode_num)
+        self.assertEqual("4.2.0.69\0\0\0\0\0\0", pkt.version_string)
+
+        rt_got = SessionStartPacket.from_bytes(got)
+        self.assert_headers_equal(pkt.header, rt_got.header, "SessionStartPacket")
+        self.assertEqual(pkt.host_id, rt_got.host_id)
+        self.assertEqual(pkt.mode_num, rt_got.mode_num)
+        self.assertEqual(pkt.version_string, rt_got.version_string)
 
     def test_config_packet(self):
         self._test_trivial_packet(ConfigGetPacket,
@@ -177,7 +187,7 @@ class TestPacket(unittest.TestCase):
 
     def test_device_reset_packet(self):
         expected = bytes.fromhex("bbbb0b000000000000000000341200001900000000004f8d00")
-        pkt = DeviceResetPacket(0x1234, 0)        
+        pkt = DeviceResetPacket(0x1234, 0)
         got = pkt.to_bytes()
         self.maybe_dump_bufs(expected, got)
         self.assertEqual(expected, got)
@@ -186,11 +196,10 @@ class TestPacket(unittest.TestCase):
         self.assert_headers_equal(pkt.header, rt_got.header, pkt.__class__)
         self.assertEqual(pkt.value, rt_got.value)
 
-
     def test_tech_info_packet(self):
         self._test_trivial_packet(TechnicalStatusPacket,
                                   "bbbb1500000000000000000034120000180000000000af7d")
-        
+
         expected = bytes.fromhex("bbbb1500000000000000000034120000180000000000af7d")
         pkt = TechnicalStatusPacket(0x1234)
         got = pkt.to_bytes()
@@ -201,7 +210,7 @@ class TestPacket(unittest.TestCase):
 
     def test_led_packet(self):
         expected = bytes.fromhex("bbbb230098badc0e0000000078563412190000000000fba900")
-        pkt = LEDPacket(0x12345678, 0, 0x0edcba98)        
+        pkt = LEDPacket(0x12345678, 0, 0x0edcba98)
         got = pkt.to_bytes()
         self.maybe_dump_bufs(expected, got)
         self.assertEqual(expected, got)
@@ -210,7 +219,6 @@ class TestPacket(unittest.TestCase):
         self.assert_headers_equal(pkt.header, rt_got.header, "LEDPacket")
         self.assertEqual(pkt.value, rt_got.value)
 
-        
     def test_is_paired_packet(self):
         expected = bytes.fromhex("bbbb2a000000000000000000000000001800000000006444")
         pkt = IsDevicePairedPacket(0)
@@ -220,7 +228,6 @@ class TestPacket(unittest.TestCase):
 
         rt_got = IsDevicePairedPacket.from_bytes(got)
         self.assert_headers_equal(pkt.header, rt_got.header, pkt.__class__)
-
 
     def test_is_paired_response_packet(self):
         buf = bytes.fromhex("bbbb2b000000000000000000000000001d0000000000ff102a00000000")
@@ -232,47 +239,19 @@ class TestPacket(unittest.TestCase):
         self.assertEqual(pkt.header.crc, 0x10ff)
         self.assertEqual(pkt.value, 0)
         self.assertEqual(pkt.header.response, 0)
-        
-        
-    def _test_from_bytes(self):
-        buf = bytes.fromhex("bbbb230098badc0e00000000785634121900000000003a2f00")
-        
-        pkt = KindPacket.from_bytes(buf)
 
-        self.assertEqual(pkt.header.kind, 0x23)
-        self.assertEqual(pkt.header.timestamp, 0x0edcba98)
-        self.assertEqual(pkt.header.seqno, 0x12345678)
-        self.assertEqual(pkt.header.length, 25)
-        self.assertEqual(pkt.payload(), bytes([0]))
+    def test_get_log_file_packet(self):
+        expected = bytes.fromhex("bbbb440000000000000000003412000020000000000014ce6300000000080000")
+        pkt = LogGetPacket(0x1234, 99, 2048)
+        got = pkt.to_bytes()
+        self.maybe_dump_bufs(expected, got)
+        self.assertEqual(expected, got)
 
-    def _test_ackpacket_from_bytes__is_paired(self):
-        buf = bytes.fromhex("bbbb00000000000000000000000000001d0000000000d73f2a000100000000000000000000000000")
+        rt_got = LogGetPacket.from_bytes(got)
+        self.assert_headers_equal(pkt.header, rt_got.header, pkt.__class__)
+        self.assertEqual(pkt.offset, rt_got.offset)
+        self.assertEqual(pkt.reqlen, rt_got.reqlen)
 
-        pkt = AckPacket.from_bytes(buf)
-#         print(pkt)
-#         print(pkt.kind, pkt.dep)
-        self.assertEqual(pkt.header.kind, 0x0000)
-        self.assertEqual(pkt.header.timestamp, 0)
-        self.assertEqual(pkt.header.seqno, 0)
-        self.assertEqual(pkt.header.crc, 0x3fd7)
-
-        self.assertEqual(pkt.orig_kind, 0x2a)
-        self.assertEqual(pkt.status, 1)
-
-
-    def _test_from_bytes__session_start(self):
-        buf = bytes.fromhex("BBBB01002EE8DB6400000000030000002C00000000007D0730323A3001390000000000000000000000000000")
-
-        pkt = SessionStartPacket.from_bytes(buf)
-        self.assertEqual(pkt.header.kind, 0x0001)
-        self.assertEqual(pkt.header.timestamp, 39612)
-        self.assertEqual(pkt.header.seqno, 3)
-        self.assertEqual(pkt.header.crc, 0x077d)
-        self.assertEqual(pkt.host_id, 0x30323a30)
-        self.assertEqual(pkt.mode_num, 1)
-        self.assertEqual(pkt.version_string, "9" + "\0"*13)
-
-        
 
 if __name__ == '__main__':
     unittest.main()
